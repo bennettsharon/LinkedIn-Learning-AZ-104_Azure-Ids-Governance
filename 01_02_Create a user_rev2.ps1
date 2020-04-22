8ece6c421288325539fe4179ce7e0d35ab6d3b38
bennettsharon <sbennett@linkedin.com>
2020-04-22 19:32:45 -0400

    Add files via upload

---
+++ 01_02_Create a user_rev2.ps1
@@ -0,0 +1,16 @@
+﻿#Create Azure AD User
+
+#Create Password Profile
+
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.Password = "VirtualMachine123!"
+
+
+#Creates the user
+New-AzureADUser -DisplayName "Wanda Smith"`
+-PasswordProfile $PasswordProfile -UserPrincipalName "" `
+-AccountEnabled $true -MailNickName "Wanda" -Department "production"
+
+
+#Delete user
+Remove-AzureADUser -ObjectId ""
---
+++ 01_04_Create a group_rev2.ps1
@@ -0,0 +1,15 @@
+﻿#Create a Group
+New-AzureADGroup -DisplayName "Dev-Prod" -MailEnabled $false -SecurityEnabled $true -MailNickName "Dev-Prod"
+
+#Add a member to a group
+
+Get-AzureADUser -objectID "user@yourcompany.com" 
+
+Add-AzureADGroupMember -ObjectId "" -RefObjectId ""
+
+#View group members
+
+Get-AzureADGroupMember -All $true -ObjectId ""
+
+#Delete Group
+Remove-AzureADGroup -ObjectId ""
