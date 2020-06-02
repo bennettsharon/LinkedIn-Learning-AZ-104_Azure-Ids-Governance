5d139cf3fa97803a1ef1dd89918caca3642fff34
bennettsharon <sbennett@linkedin.com>
2020-06-02 10:01:04 -0400

    Add files via upload

---
+++ 02_02_RoleAssignments.ps1
@@ -0,0 +1,15 @@
+﻿$Name = 'username'
+$RoleDef = 'Experiment Operator'
+$RG = 'resource group'
+
+#List role assignments for current subscription
+Get-AZRoleAssignment
+
+#List role assignments for a user
+Get-AzRoleAssignment -SignInName $Name
+
+#Add role assignment RG
+New-AzRoleAssignment -SignInName $Name -RoleDefinitionName $RoleDef -ResourceGroupName $RG
+
+#Delete role assignmnet 
+Remove-AzRoleAssignment -SignInName $Name -RoleDefinitionName $RoleDef -ResourceGroupName $RG
